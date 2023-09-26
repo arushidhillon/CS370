25de3858b3fa48e6be242e067fcfa7e2c645336b
arushidhillon <82238287+arushidhillon@users.noreply.github.com>
2023-09-26 17:54:55 -0400

    Login/Profile Page
    Login Page, completed. Profile page is half done. Index.html, index.js, and style.css are all for the login page. Profile.css, and StudentMain.html are for the profile page.

---
+++ Index.js
@@ -0,0 +1,25 @@
+let signup = document.querySelector(".signup");
+let student = document.querySelector(".student");
+let lab = document.querySelector(".lab");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+ 
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider2");
+    formSection.classList.add("form-section-move2");
+});
+ 
+student.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    slider.classList.remove("moveslider2");
+    formSection.classList.remove("form-section-move");
+    formSection.classList.remove("form-section-move2");
+});
+
+lab.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    slider.classList.remove("moveslider2");
+    formSection.classList.add("form-section-move");
+    formSection.classList.remove("form-section-move2");
+
+});
