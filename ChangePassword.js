b66668cbbae59e27e7f2f1c4d49710025108b80b
Arushi Dhillon <arushidhillon@Arushis-MacBook-Pro.local>
2023-10-02 15:20:47 -0400

    Change Password Page added

---
+++ ChangePassword.js
@@ -0,0 +1,13 @@
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirmPassword");
+enableSubmitButton();
+
+function validatePassword() {
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+    return false;
+  } else {
+    confirm_password.setCustomValidity('');
+    return true;
+  }
+}
