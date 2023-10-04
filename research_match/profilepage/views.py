81f59e12cb7bf7763c1ca89b1458cc99cab47f95
ArushiDhillon <arushi.dhillon@emory.edu>
2023-10-04 12:28:17 -0400

    testing forms

--- research_match/profilepage/views.py
+++ research_match/profilepage/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
-
 from django.http import HttpResponse
 from django.template import loader
+
+from django.views.decorators.csrf import ensure_csrf_cookie
+@ensure_csrf_cookie
 
 def hi(request):
     template = loader.get_template('StudentMain.html')
     return HttpResponse(template.render())
+
+from .forms import SkillForm
+
+
+def get_skill(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = SkillForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = SkillForm()
+
+    return render(request, "skill.html", {"form": form})
