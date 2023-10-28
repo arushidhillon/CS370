6db423f72f715722b80320cbf005d1deaef3493f
daniellee3684 <143568365+daniellee3684@users.noreply.github.com>
2023-10-28 15:22:42 -0400

    Delete research_match/admin.py

--- research_match/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-class InboxMessageAdmin(admin.ModelAdmin):
-    readonly_fields = ('sender', 'conversation', 'body')
-
-admin.site.register(InboxMessage, InboxMessageAdmin)
-admin.site.register(Conversation)
