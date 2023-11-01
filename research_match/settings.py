c4a9a3398ff75a27d21be99104f6869e01b01085
DevAGee <devingee11@gmail.com>
2023-10-31 21:53:27 -0400

    Update settings.py

--- research_match/settings.py
+++ research_match/settings.py
@@ -89,18 +89,6 @@ WSGI_APPLICATION = 'research_match.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   #     'NAME': '<DATABASE>',
-    #    'USER': '<USER>',
-     #   'PASSWORD': '<PASSWORD>',
-      #  'HOST': '<HOST>',
-       # 'PORT': '5432',
-#    }
-#}
 DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
@@ -146,7 +134,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
