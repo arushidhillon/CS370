7beabe7c245597c4a6d377eac9daeb7c0e1f277e
adrianvalencia1 <107940199+adrianvalencia1@users.noreply.github.com>
2023-11-01 10:55:06 -0400

    Loginpage styling

--- research_match/profilepage/static/js/loginpage.js
+++ research_match/profilepage/static/js/loginpage.js
@@ -3,24 +3,46 @@ let student = document.querySelector(".student");
 let lab = document.querySelector(".lab");
 let slider = document.querySelector(".slider");
 let formSection = document.querySelector(".form-section");
- 
+
 signup.addEventListener("click", () => {
-    slider.classList.add("moveslider2");
-    formSection.classList.add("form-section-move2");
-});
- 
-student.addEventListener("click", () => {
+  if (lab.hasAttribute("id")) {
+    lab.removeAttribute("id");
+  }
+  if (student.hasAttribute("id")) {
+    student.removeAttribute("id");
+  }
+  signup.setAttribute("id", "selected");
+  if (slider.classList.contains("moveslider")) {
     slider.classList.remove("moveslider");
-    slider.classList.remove("moveslider2");
-    formSection.classList.remove("form-section-move");
-    formSection.classList.remove("form-section-move2");
+  }
+  slider.classList.add("moveslider2");
+  formSection.classList.add("form-section-move2");
+});
+
+student.addEventListener("click", () => {
+  if (lab.hasAttribute("id")) {
+    lab.removeAttribute("id");
+  }
+  if (signup.hasAttribute("id")) {
+    signup.removeAttribute("id");
+  }
+  student.setAttribute("id", "selected");
+  slider.classList.remove("moveslider");
+  slider.classList.remove("moveslider2");
+  formSection.classList.remove("form-section-move");
+  formSection.classList.remove("form-section-move2");
 });
 
 lab.addEventListener("click", () => {
-    slider.classList.add("moveslider");
-    slider.classList.remove("moveslider2");
-    formSection.classList.add("form-section-move");
-    formSection.classList.remove("form-section-move2");
-
+  if (student.hasAttribute("id")) {
+    student.removeAttribute("id");
+  }
+  if (signup.hasAttribute("id")) {
+    signup.removeAttribute("id");
+  }
+  lab.setAttribute("id", "selected");
+  slider.classList.add("moveslider");
+  slider.classList.remove("moveslider2");
+  formSection.classList.add("form-section-move");
+  formSection.classList.remove("form-section-move2");
 });
-
