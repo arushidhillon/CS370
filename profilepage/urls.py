545a255690c9084fd35291b42ecaa3853cd7b306
DevAGee <devingee11@gmail.com>
2023-10-31 20:00:04 -0400

    Clean up url pages

--- profilepage/urls.py
+++ profilepage/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+
 from . import views
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
 
 
 
