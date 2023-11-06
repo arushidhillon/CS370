cb2862e2ab1042b2c047034e3fbec523cdf88110
Daniel Lee <daniel.lee2@emory.edu>
2023-11-06 02:20:00 -0500

    search

--- research_match/search/admin.py
+++ research_match/search/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
-from .models import *
+from .models import SearchQuery
+
+class SearchQueryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'query', 'timestamp')
+    search_fields = ('user__username', 'query')
+    list_filter = ('timestamp',)
+    date_hierarchy = 'timestamp'
+
+admin.site.register(SearchQuery, SearchQueryAdmin)
