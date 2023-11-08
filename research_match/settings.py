f11c1e79dc66ce71cc86f71b95440962317fa02d
DevAGee <devingee11@gmail.com>
2023-11-08 09:36:42 -0500

    Update settings.py

--- research_match/settings.py
+++ research_match/settings.py
@@ -124,3 +124,20 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+AUTH_USER_MODEL = 'profilepage.User'
