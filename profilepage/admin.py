8f3e201a3d088e0bb2d5dd0f39073743258e3be8
DevAGee <devingee11@gmail.com>
2023-11-08 09:21:25 -0500

    Update admin.py

--- profilepage/admin.py
+++ profilepage/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import StudentProfile, Mentor
+from .models import StudentProfile
 
 # Register your models here.
 
 admin.site.register(StudentProfile)
-admin.site.register(Mentor)
