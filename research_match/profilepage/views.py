63f2efc6672dbc403ed04100c995b2de3c2e70fe
yari-gut <ygutierrezyadi@gmail.com>
2023-11-07 19:42:26 -0500

    Deleted functions that causes error, created a navbar html page, and created decorators to differentiate between users

--- research_match/profilepage/views.py
+++ research_match/profilepage/views.py
@@ -26,7 +26,9 @@ from django.utils.encoding import force_str, force_bytes
 from . tokens import generate_token
 from django.contrib.auth.password_validation import validate_password
 from .forms import UserUpdateForm, ProfileUpdateForm
-
+from .decorators import allowed_users, unauthenticated_user
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 from django.views.decorators.csrf import ensure_csrf_cookie
 @ensure_csrf_cookie
 
@@ -58,17 +60,23 @@ def signup(request):
             messages.error(request, "Email already exists! Please try some other email")
             return redirect('signup')
         
-        #checks to make sure password is 8 characters long
-        # if validate_password(pass1) is not None:
-        #    messages.error(request, "Password must be 8 characters long. They cannot be entirely numerical/alphabetical.")
-           
+        #checks to make sure password is 8 characters long, not entirely numerical/alphabetical, or too common
+        if pass1 is not None:
+            try:
+                validate_password(pass1)
+            except ValidationError as e:
+    
+                for error in e.error_list:
+                    messages.error(request, error)
+
+            return redirect('signup')
 
         #makes sure password and confirmation password match
         if pass1 != pass2:
             messages.error(request, "Passwords didn't match!")
             return redirect('signup')
         
-       # if 'stdbtn' in request.POST:
+
        # if 'labbtn' in request.POST:
         
         
@@ -79,6 +87,14 @@ def signup(request):
         myuser.is_active = False
 
         myuser.save()
+
+        # if 'stdbtn' in request.POST:
+        #     group = Group.objects.get(name='student')
+        #     myuser.groups.add(group)
+
+        # if 'labbtn' in request.POST:
+        #     group = Group.objects.get(name='mentor')
+        #     myuser.groups.add(group)
 
         messages.success(request,"Your Account has been successfully created. We have sent you a confirmation email, please confirm your email in order to activate your account. You may need to look in the spam folder.")
 
@@ -135,6 +151,7 @@ def activate(request, uidb64, token):
         return render(request, 'activation_failed.html')
     
 # This function logs the user in after they activate their account.
+@allowed_users(allowed_roles=['student'])
 def studentlogin(request):
 
     if request.method == 'POST':
@@ -142,7 +159,7 @@ def studentlogin(request):
         password = request.POST['password']
 
         user = authenticate(username=email, password=password)
-        print(user)
+       
         if user is not None:
             if user.is_active:
                 login(request, user)
@@ -168,6 +185,7 @@ def studentlogin(request):
     return render(request, "registration/loginpage.html")
 
 #same code as student login, doesn't lead to lab profile page or stores in different table for now
+@allowed_users(allowed_roles=['mentor'])
 def lablogin(request):
 
     if request.method == 'POST':
@@ -179,19 +197,23 @@ def lablogin(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                firstname = user.first_name
+                # This redirects the user to the login page if they aren't loggged in and are in a different page.
                 if 'next' in request.POST:
                     return redirect(request.POST.get('next'))
                 else:
-                    return render(request, "StudentMain.html", {'firstname': firstname})
+                    return render(request, "LabMain.html")
             
             else:
                 messages.error(request, "User account is not confirmed. Please check your email for confirmation link.")
-                return redirect('signup')
+                return redirect('lablogin')
 
         else:
-            messages.error(request, "Bad Credentials!")
-            return redirect('signup')
+            if User.objects.filter(email=email).exists():
+                messages.error(request, "The Password you entered is incorrect. Please try again or reset password.")
+                return redirect('lablogin')
+            else:
+                messages.error(request, "The email you entered does not match our records. Please double-check and try again.")
+                return redirect('lablogin')
 
 
     return render(request, "registration/loginpage.html")
