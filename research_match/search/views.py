96bccbbd18aef5168620d61554b6f1fe282796ad
daniellee3684 <143568365+daniellee3684@users.noreply.github.com>
2023-11-24 20:55:49 -0800

    search

--- research_match/search/views.py
+++ research_match/search/views.py
@@ -3,30 +3,27 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from profilepage.models import StudentProfile
 
-def search(request):
-    return render(request, 'search.html')
-
-def search_profiles(request):
-    if request.htmx:  # Check for the HTMX request header
-        query = request.GET.get('q', '') 
-
-        if len(query) > 0:
-            # Search profiles by matching the query with first name, last name, or background fields
-            profiles = StudentProfile.objects.filter(
-                Q(firstname__icontains=query) | 
-                Q(lastname__icontains=query) | 
-                Q(background__icontains=query) |
-                Q(labname__icontains=query)
-            )
-            # Render only the search results to a specific template for HTMX to swap in
-            return render(request, 'search/list_profiles.html', {'profiles': profiles})
+def search_students(request):
+    if request.htmx:
+        letters = request.GET.get('search_students')
+        if len(letters) > 0:
+            students = StudentProfile.objects.filter(first_name__contains=letters).exclude(username=request.user)
+            return render(request, 'list_searchuser.html', { 'students' : students })
         else:
-            # If query is empty, return an empty response to clear the results
             return HttpResponse('')
-
     else:
-        # If it's not an HTMX request, raise a 404
-        raise Http404('')
+        raise Http404()
+
+def search_labs(request):
+    if request.htmx:
+        letters = request.GET.get('search_labs')
+        if len(letters) > 0:
+            labs = StudentProfile.objects.filter(labname__contains=letters).exclude(username=request.user)
+            return render(request, 'list_searchuser.html', { 'labs' : labs })
+        else:
+            return HttpResponse('')
+    else:
+        raise Http404()
 
 #Function to display the details of the profile
 def profile_detail(request, id):
