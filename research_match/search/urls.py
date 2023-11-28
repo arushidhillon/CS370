e7d92b70bc6bc58d6557a3a58465a3f177591e6e
daniellee3684 <143568365+daniellee3684@users.noreply.github.com>
2023-11-28 03:36:02 -0500

    search

--- research_match/search/urls.py
+++ research_match/search/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # URL for rendering the search page
+    path('search',all_students, name='all_students'),
+    
     path('search-students/',search_students, name='search_students'),
-
-    # URL for processing the search query and returning the results
 
 ]
