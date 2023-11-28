ae7573d679bd8b055f6a6e05f69e2a8ea97750dc
DevAGee <devingee11@gmail.com>
2023-11-28 16:46:01 -0500

    Revert "Update info.py"
    This reverts commit 62fced74db2731b85968d91f3849b36902ef22a8.

--- research_match/info.py
+++ research_match/info.py
@@ -1,10 +1,5 @@
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'mail.gmx.com'  # GMX SMTP server
-EMAIL_HOST_USER = 'match.research@gmx.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'researchmatchdemo@gmail.com'
+EMAIL_HOST_PASSWORD = 'cbidqinxyqdklmhn'
 EMAIL_PORT = 587
-
-
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'match.research@gmx.com'
-EMAIL_HOST_PASSWORD = 'YJNCDX5JZJWCN45VEG5Z'
