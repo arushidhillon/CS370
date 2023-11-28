f79edbdd2edb5c573992a995d2276365d8cfd7bd
DevAGee <devingee11@gmail.com>
2023-11-28 07:34:20 -0500

    Rollback and Test

--- research_match/urls.py
+++ research_match/urls.py
@@ -20,4 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include("profilepage.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('',include("inbox.urls")),
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
