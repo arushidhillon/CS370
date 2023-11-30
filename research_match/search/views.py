165260194b77d8061a8d44b9545da19d46156810
daniellee3684 <143568365+daniellee3684@users.noreply.github.com>
2023-11-29 21:16:48 -0500

    search

--- research_match/search/views.py
+++ research_match/search/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def all_students(request):
     all = User.objects.all()
-    all_students = all.filter(groups ='2')  # Retrieve all users
+    all_students = all.filter(group ='2')  # Retrieve all users
 
     return render(request, 'students.html', {'students': all_students})
 
@@ -17,7 +17,7 @@ def search_students(request):
     if request.htmx:
         letters = request.GET.get('search_students')
         if len(letters) > 0:
-            profiles = User.objects.filter(first_name__contains=letters, groups = '2').exclude(username=request.user)
+            profiles = User.objects.filter(first_name__contains=letters, group = '2').exclude(username=request.user)
             return render(request, 'list_students.html', { 'users' : profiles })
         else:
             return HttpResponse('')
