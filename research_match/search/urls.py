2f5137e0d0aa5755dd78abcded1b4ea1f15503c9
daniellee3684 <143568365+daniellee3684@users.noreply.github.com>
2023-11-29 20:55:58 -0500

    search

--- research_match/search/urls.py
+++ research_match/search/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('search',all_students, name='all_students'),
+    path('students',all_students, name='all_students'),
     
-    path('search-students/',search_students, name='search_students'),
+    path('search_students/',search_students, name='search_students'),
 
 ]
