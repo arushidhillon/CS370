4d38f0e60abcdc0c803d7ef803639de5da982f70
daniellee3684 <143568365+daniellee3684@users.noreply.github.com>
2023-11-30 20:23:01 -0500

    changes

--- research_match/search/views.py
+++ research_match/search/views.py
@@ -1,24 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from profilepage.models import User
-from django.db.models import Q
-from django.core.paginator import Paginator
-from django.contrib.auth.decorators import login_required
+from profilepage.decorators import allowed_users
 
-@login_required
-def all_students(request):
-    all = User.objects.all()
-    all_students = all.filter(groups ='2')  # Retrieve all users
 
-    return render(request, 'students.html', {'students': all_students})
+@allowed_users(allowed_roles=['lab'])
+def students(request):
+    return render(request, 'students.html')
 
-@login_required
+
+
 def search_students(request):
+    all = User.objects.all()
+    all_students = all.filter(groups__name ='student')
+
     if request.htmx:
         letters = request.GET.get('search_students')
+        if len(letters) > 0 :
+            profiles = User.objects.filter(first_name__contains=letters).exclude(username=request.user)
+            return render(request, 'list_students.html', {'students' : profiles})
+        else:
+            return HttpResponse('')
+    else:
+        raise Http404()
+
+
+@allowed_users(allowed_roles=['student'])
+def opportunities(request):
+    return render(request, "opportunities.html")
+
+
+@allowed_users(allowed_roles=['student'])
+def search_opportunities(request): 
+    all = User.objects.all()
+    all_opportunities = all.filter(groups__name='lab')
+
+    if request.htmx:
+        letters = request.GET.get('search_labs')
         if len(letters) > 0:
-            profiles = User.objects.filter(first_name__contains=letters, groups = '2').exclude(username=request.user)
-            return render(request, 'list_students.html', { 'users' : profiles })
+            labs = all_opportunities.filter(background__contains=letters | all_opportunities.filter(labname__contains=letters) | all_opportunities.filter(skills__contains=letters)).exclude(username=request.user)
+            return render(request, 'list_opportunities.html', {'labs' : labs})
         else:
             return HttpResponse('')
     else:
