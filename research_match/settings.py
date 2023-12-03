2dcbd69446e4a4ad0c570473e06c1667cdd740f1
DevAGee <devingee11@gmail.com>
2023-12-03 13:05:21 -0500

    Update settings.py

--- research_match/settings.py
+++ research_match/settings.py
@@ -22,23 +22,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-if DEBUG:
-    SECRET_KEY = 'django-insecure-9h#qg6t@pxdf3&nypr1k%!a!8myas-g)26*&!bo#wy#3#-85)h'
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+#if DEBUG:
+#    SECRET_KEY = 'django-insecure-9h#qg6t@pxdf3&nypr1k%!a!8myas-g)26*&!bo#wy#3#-85)h'
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': BASE_DIR / 'db.sqlite3',
+#        }
+#    }
     
-else:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
-    db_from_env = dj_database_url.config(conn_max_age=600)
-    DATABASES['default'].update(db_from_env)
-
+#else:
+#    SECRET_KEY = os.environ.get('SECRET_KEY')
+#    DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
+#    db_from_env = dj_database_url.config(conn_max_age=600)
+#    DATABASES['default'].update(db_from_env)
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 ENCRYPT_KEY = b'WIuRycBTSZ9VVevuPE4kXdnwVUlVrC7p1qZTDgFx-Sc='
 
