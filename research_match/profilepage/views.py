88a89cc246f1d0ec1b2cac278fa828ea5c6b44c3
ArushiDhillon <arushi.dhillon@emory.edu>
2023-12-04 11:42:38 -0500

    gpa form added

--- research_match/profilepage/views.py
+++ research_match/profilepage/views.py
@@ -31,7 +31,7 @@ from .forms import UserUpdateForm, ProfileUpdateForm
 from .decorators import allowed_users, unauthenticated_user
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
-from .forms import UserUpdateForm, ProfileUpdateForm, LabUpdateForm, Picform, Skillform, Courseform, Docform, BioForm
+from .forms import UserUpdateForm, ProfileUpdateForm, LabUpdateForm, Picform, Skillform, Courseform, Docform, BioForm, GpaForm
 from django.views.generic import ListView
 import random
 
@@ -440,6 +440,46 @@ def studentskillsupdate(request):
         
         else:
             p_form = Skillform(instance=request.user.studentprofile)
+
+
+        context = {
+            'p_form': p_form
+        }
+        return render(request, 'editprofilepic.html', context)
+
+def labgpaupdate(request):
+    if request.method == 'POST':
+        p_form = GpaForm(request.POST, 
+                                   instance=request.user.studentprofile)
+       # if request.FILES.get('profile_pic') is None:
+    #     if pic_form.is_valid():
+        if p_form.is_valid():
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect(f'profile/{request.user.studentprofile.user.email.split("@")[0]}')  # Send back to profile 
+        
+        else:
+            p_form = GpaForm(instance=request.user.studentprofile)
+
+
+        context = {
+            'p_form': p_form
+        }
+        return render(request, 'editprofilepic.html', context)
+    
+def studentgpaupdate(request):
+    if request.method == 'POST':
+        p_form = GpaForm(request.POST, instance=request.user.studentprofile)
+       # if request.FILES.get('profile_pic') is None:
+    #     if pic_form.is_valid():
+        if p_form.is_valid():
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            # TODO: apply to other forms
+            return redirect(f'profile/{request.user.studentprofile.user.email.split("@")[0]}')  # Send back to profile
+        
+        else:
+            p_form = GpaForm(instance=request.user.studentprofile)
 
 
         context = {
