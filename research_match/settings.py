aec9d12ebd342e742ccbefe3f8cdbcef047d0b2b
DevAGee <devingee11@gmail.com>
2023-12-03 19:16:11 -0500

    Update settings.py

--- research_match/settings.py
+++ research_match/settings.py
@@ -41,7 +41,7 @@ else:
     # Keys
     SECRET_KEY = os.environ.get('SECRET_KEY')
     ENCRYPT_KEY = os.environ.get('ENCRYPT_KEY')
-
+    ENCRYPT_KEY = b'WIuRycBTSZ9VVevuPE4kXdnwVUlVrC7p1qZTDgFx-Sc='
     DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
     db_from_env = dj_database_url.config(conn_max_age=600)
     DATABASES['default'].update(db_from_env)
