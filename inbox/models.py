85268e6b6d403b65f90b5ab28699a04f583e8102
daniellee3684 <143568365+daniellee3684@users.noreply.github.com>
2023-12-03 22:32:04 -0500

    added comments

--- inbox/models.py
+++ inbox/models.py
@@ -6,36 +6,42 @@ from cryptography.fernet import Fernet
 from django.conf import settings
 import uuid
 
+# Model for individual messages in the inbox system
 class InboxMessage(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
-    conversation = models.ForeignKey('Conversation', on_delete=models.CASCADE, related_name="messages")
-    body = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages") # Reference to the user who sent the message
+    conversation = models.ForeignKey('Conversation', on_delete=models.CASCADE, related_name="messages")  # Reference to the conversation the message belongs to
+    body = models.TextField() # The body of the message
+    created = models.DateTimeField(auto_now_add=True) # Timestamp when the message was created
 
+    # Method to decrypt the message body
     @property
     def body_decrypted(self):
-        f = Fernet(settings.ENCRYPT_KEY)
-        message_decrypted = f.decrypt(self.body)
-        message_decoded = message_decrypted.decode('utf-8')
+        f = Fernet(settings.ENCRYPT_KEY) # Encryption key from settings
+        message_decrypted = f.decrypt(self.body) # Decrypt the message body
+        message_decoded = message_decrypted.decode('utf-8') # Decode the decrypted message
         return message_decoded
     
+    # Metadata options for the InboxMessage model
     class Meta:
-        ordering = ['-created']
-        
+        ordering = ['-created'] # Orders messages by their creation time, newest first
+
+    # String representation of the InboxMessage model  
     def __str__(self):
-        time_since = timesince(self.created, timezone.now())
-        return f'[{self.sender.username} : {time_since} ago]'
+        time_since = timesince(self.created, timezone.now()) # Time since the message was sent
+        return f'[{self.sender.username} : {time_since} ago]' # Format: [sender: time since sent ago]
     
-    
+# Model representing a conversation between users
 class Conversation(models.Model):
-    id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    participants = models.ManyToManyField(User, related_name='conversations')
-    lastmessage_created = models.DateTimeField(default=timezone.now)
-    is_seen = models.BooleanField(default=False)
+    id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False) # Unique ID for the conversation
+    participants = models.ManyToManyField(User, related_name='conversations') # Users participating in the conversation
+    lastmessage_created = models.DateTimeField(default=timezone.now)  # Timestamp of the last message sent in the conversation
+    is_seen = models.BooleanField(default=False) # Flag to indicate if the last message has been seen
     
+    # Metadata options for the Conversation model
     class Meta:
-        ordering = ['-lastmessage_created']
+        ordering = ['-lastmessage_created'] # Orders conversations based on the latest message sent
         
+    # String representation of the Conversation model
     def __str__(self):
-        user_names = ", ".join(user.username for user in self.participants.all())
-        return f'[{user_names}]'
+        user_names = ", ".join(user.username for user in self.participants.all()) # Join usernames of participants
+        return f'[{user_names}]' # Format: [username1, username2, ...]
