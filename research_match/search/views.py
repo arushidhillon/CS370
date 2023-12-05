70b4e7a04c4002566ac4811780a9c2096507884b
daniellee3684 <143568365+daniellee3684@users.noreply.github.com>
2023-12-04 20:52:02 -0500

    added comments

--- research_match/search/views.py
+++ research_match/search/views.py
@@ -3,45 +3,24 @@ from django.http import HttpResponse, Http404
 from profilepage.models import User
 from profilepage.decorators import allowed_users
 
-
+# This decorator restricts access to the 'students' view to users with the 'lab' role.
 @allowed_users(allowed_roles=['lab'])
 def students(request):
     return render(request, 'students.html')
 
 
-
+# This function handles the searching of students.
 def search_students(request):
-    all = User.objects.all()
-    all_students = all.filter(groups__name ='student')
+    all = User.objects.all() # Retrieves all User objects from the database.
+    all_students = all.filter(groups__name ='student') # Filters the User objects to include only those in the 'student' group.
 
     if request.htmx:
         letters = request.GET.get('search_students')
-        if len(letters) > 0 :
-            profiles = all_students.objects.filter(first_name__icontains=letters).exclude(username=request.user)
+        if len(letters) > 0 : # Checks if the search input is not empty.
+            profiles = all_students.objects.filter(first_name__icontains=letters).exclude(username=request.user) # Filters the student profiles based on the search input in the first name field and excludes the current user from the results.
             return render(request, 'list_students.html', {'students' : profiles})
         else:
-            return HttpResponse('')
-    else:
-        raise Http404()
-
-
-@allowed_users(allowed_roles=['student'])
-def opportunities(request):
-    return render(request, "opportunities.html")
-
-
-@allowed_users(allowed_roles=['student'])
-def search_opportunities(request): 
-    all = User.objects.all()
-    all_opportunities = all.filter(groups__name='lab')
-
-    if request.htmx:
-        letters = request.GET.get('search_labs')
-        if len(letters) > 0:
-            labs = all_opportunities.filter(background__icontains=letters | all_opportunities.filter(labname__icontains=letters) | all_opportunities.filter(skills__icontains=letters)).exclude(username=request.user)
-            return render(request, 'list_opportunities.html', {'labs' : labs})
-        else:
-            return HttpResponse('')
+            return HttpResponse('') # Returns an empty HttpResponse if there is no search input.
     else:
         raise Http404()
 
