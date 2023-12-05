9dd680d24c8a61294758af7b598a05d072d7d252
daniellee3684 <143568365+daniellee3684@users.noreply.github.com>
2023-12-04 20:54:19 -0500

    added comments

--- research_match/search/models.py
+++ research_match/search/models.py
@@ -3,10 +3,10 @@ from django.conf import settings
 from django.utils import timezone
 
 class SearchQuery(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='search_queries')
-    query = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='search_queries') # Link to the user who made the search.
+    query = models.CharField(max_length=255) # Text field to store the search query.
+    timestamp = models.DateTimeField(default=timezone.now) # Time when the search was made.
 
     def __str__(self):
-        return f"{self.user.username} searched for '{self.query}' at {self.timestamp}"
+        return f"{self.user.username} searched for '{self.query}' at {self.timestamp}" # Shows who searched what and when.
 
