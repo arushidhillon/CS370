f708f850b52f24aab2828ae45d721e61c95efcf6
DevAGee <devingee11@gmail.com>
2023-12-04 20:04:00 -0500

    Update decorators.py

--- profilepage/decorators.py
+++ profilepage/decorators.py
@@ -18,3 +18,12 @@ def allowed_users(allowed_roles=[]):
             return view_func(request, *args, **kwargs)
         return wrapper_func
     return decorator
+
+
+def unauthenticated_user(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return view_func(request, *args, **kwargs)
+
+    return wrapper_func
